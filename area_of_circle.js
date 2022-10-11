aed36358d8d9c795e3d68598dc2b9b20fb252e6d
Penchala Vamsi Chebrolu <69138538+penchalavamsic@users.noreply.github.com>
2022-10-11 21:34:48 +0530

    JS

---
+++ area_of_circle.js
@@ -0,0 +1,6 @@
+let radius =2
+const pi= 3.14 
+//const is a keyword which is used for constant values that will not change through out the program
+let area
+area= pi*radius*radius
+console.log(area)
---
+++ variable.js
@@ -0,0 +1,17 @@
+num= 4+6
+console.log(num)
+v= num+10
+console.log(v)
+
+//we should use "let" before declaring a variable in js as per the new versions.
+let h=2
+console.log(2+h)
+
+//string should be declare in "" and '' to print the input
+let username="vamsi"
+console.log(username)
+
+//$ and _ are the only special symbols that can be used in js
+//variable name can be combiantion of nums and chars, but starts with chars only
+let $na_me="hello"
+console.log($na_me)
